783d44933b3a8d9d329c7acc0530fa7dda6e7515
Chigbu Joshua <chigbujoshua@yahoo.com>
2024-01-15 02:12:37 +0100

    Unittests

--- tests/main.py
+++ tests/main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ Test File """
-import sys
-sys.path.append("..")
 from models.base_model import BaseModel
 from models import storage
 from uuid import uuid4
+import sys
+sys.path.append("..")
+
 
 if __name__ == "__main__":
     all_objs = storage.all()
@@ -16,12 +17,12 @@ if __name__ == "__main__":
     print("-- Create a new object --")
     my_model = BaseModel()
     my_model.name = "My First Model"
-    my_model.my_number = 89    
+    my_model.my_number = 89
     my_model.save()
     print(my_model)
     # print(str(my_model))
     # print(my_model.__str__())
-    
+
     print()
     print("----")
     print()
@@ -39,7 +40,8 @@ if __name__ == "__main__":
     print(my_model_json)
     print("JSON of my_model:")
     for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+        print("\t{}: ({}) - {}".format(
+            key, type(my_model_json[key]), my_model_json[key]))
 
     print()
     print("----")
@@ -52,11 +54,11 @@ if __name__ == "__main__":
     print(type(my_new_model))
     print(my_model is my_new_model)
     print(my_model == my_new_model)
-    
+
     print()
     print("----")
     print()
-    
+
     new_uuid = str(uuid4())
     new_model = BaseModel(id=new_uuid, name="My third Model", my_number=9)
     print(new_model.name)
